a762eb76f4b2e480a73201f71946939a111250cc
wwt <wwtg99@126.com>
2020-01-19 12:52:15 +0800

    Add env file support in run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import click
+from envparse import env
 from application import create_app
 
 
@@ -6,7 +7,10 @@ from application import create_app
 @click.option('-h', '--host', help='Bind host', default='localhost', show_default=True)
 @click.option('-p', '--port', help='Bind port', default=8000, type=int, show_default=True)
 @click.option('-e', '--env', help='Running env, override environment FLASK_ENV.', default='development', show_default=True)
+@click.option('-f', '--env-file', help='Environment from file', type=click.Path(exists=True))
 def main(**kwargs):
+    if kwargs['env_file']:
+        env.read_envfile(kwargs['env_file'])
     app = create_app(kwargs['env'])
     app.run(host=kwargs['host'], port=kwargs['port'])
 
